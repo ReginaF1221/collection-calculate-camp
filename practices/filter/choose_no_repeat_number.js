6c2103bc1937ce5c76aad36428da0301af059c27
ReginaF1221 <reginaf1221@outlook.com>
2018-04-18 14:54:53 +0800

    filter_no_repeat

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,9 +1,27 @@
 'use strict';
-
+function includes(result,item){
+  for (let num of result ) {
+    if (num === item) {
+      return false;
+      break;
+    }
+    
+  }
+  return true;
+}
 function choose_no_repeat_number(collection) {
 
   //从collection中选出不重复的数字
-  
+  if (!collection || collection.length === 0) {
+    return;
+    let result = [];
+    for (let item of collection) {
+      if (includes(result,item)) {
+        result.push(item);
+      }
+      
+    }
+  }
 }
 
 module.exports = choose_no_repeat_number;
